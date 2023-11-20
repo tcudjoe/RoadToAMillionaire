d5fa3b610460ac57e36e4a309879b893913db84e
Tyra Cudjoe <tyra20015@gmail.com>
2023-11-20 20:11:50 +0100

    Setting up spring boot project

---
+++ RTMWebAppApi/src/main/java/hibera/api/rtmwebappapi/RtmWebAppApiApplication.java
@@ -0,0 +1,13 @@
+package hibera.api.rtmwebappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtmWebAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtmWebAppApiApplication.class, args);
+	}
+
+}
