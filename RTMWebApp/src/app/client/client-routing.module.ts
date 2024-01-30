034f0bfb62a36434aefacf54f417437bc255eb19
Tyra Cudjoe <tyra20015@gmail.com>
2024-01-30 16:18:43 +0100

    Update login button text and add two-factor authentication images and input validation. Add client layout and navbar components

--- RTMWebApp/src/app/client/client-routing.module.ts
+++ RTMWebApp/src/app/client/client-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LayoutComponent} from "../shared/components/layout/layout.component";
+import {LayoutComponent} from "./components/layout/layout.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {authGuard} from "../auth/services/authGuard";
 
 const routes: Routes = [{
   path: '', component: LayoutComponent,
   children: [
+    {
+      path: '',
+      redirectTo: 'dashboard',
+      pathMatch: 'full',
+      title: 'Road to a Millionaire | Home',
+    },
     {
       path: 'dashboard',
       component: HomeComponent,
--- RTMWebApp/src/app/client/client.module.ts
+++ RTMWebApp/src/app/client/client.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
